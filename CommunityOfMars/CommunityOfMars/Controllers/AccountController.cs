cd7da72e2c5d984956fc39bef7b507d2e3d8ffde
Angel Amores <amoresa@my.lanecc.edu>
2024-01-22 15:09:05 -0800

    Added LogIn and LogOut

--- CommunityOfMars/CommunityOfMars/Controllers/AccountController.cs
+++ CommunityOfMars/CommunityOfMars/Controllers/AccountController.cs
@@ -9,10 +9,10 @@ namespace CommunityOfMars.Controllers
         private UserManager<AppUser> userManager;
         private SignInManager<AppUser> signInManager;
 
-        public AccountController(UserManager<AppUser> userMngr)//, SignInManager<AppUser> signInMngr)
+        public AccountController(UserManager<AppUser> userMngr, SignInManager<AppUser> signInMngr)
         {
             userManager = userMngr;
-            //signInManager = signInMngr;
+            signInManager = signInMngr;
         }
 
         [HttpGet]
@@ -45,45 +45,45 @@ namespace CommunityOfMars.Controllers
             return View(model);
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> LogOut()
-        //{
-        //    await signInManager.SignOutAsync();
-        //    return RedirectToAction("Index", "Home");
-        //}
+        [HttpPost]
+        public async Task<IActionResult> LogOut()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
 
-        //[HttpGet]
-        //public IActionResult LogIn(string returnURL = "")
-        //{
-        //    var model = new LoginViewModel { ReturnUrl = returnURL };
-        //    return View(model);
-        //}
+        [HttpGet]
+        public IActionResult LogIn(string returnURL = "")
+        {
+            var model = new LoginViewModel { ReturnUrl = returnURL };
+            return View(model);
+        }
 
-        //[HttpPost]
-        //public async Task<IActionResult> LogIn(LoginViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {                
-        //        var result = await signInManager.PasswordSignInAsync(
-        //            model.Username, model.Password, isPersistent: model.RememberMe, 
-        //            lockoutOnFailure: false);
+        [HttpPost]
+        public async Task<IActionResult> LogIn(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(
+                    model.Username, model.Password, isPersistent: model.RememberMe,
+                    lockoutOnFailure: false);
 
-        //        if (result.Succeeded)
-        //        {
-        //            if (!string.IsNullOrEmpty(model.ReturnUrl) && 
-        //                Url.IsLocalUrl(model.ReturnUrl))
-        //            {
-        //                return Redirect(model.ReturnUrl);
-        //            }
-        //            else
-        //            {
-        //                return RedirectToAction("Index", "Home");
-        //            }
-        //        }
-        //    }
-        //    ModelState.AddModelError("", "Invalid username/password.");
-        //    return View(model);
-        //}
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) &&
+                        Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid username/password.");
+            return View(model);
+        }
 
         //public ViewResult AccessDenied()
         //{
@@ -107,7 +107,7 @@ namespace CommunityOfMars.Controllers
         //        AppUser user = await userManager.FindByNameAsync(model.Username);
         //        var result = await userManager.ChangePasswordAsync(user,
         //            model.OldPassword, model.NewPassword);
-                
+
         //        if (result.Succeeded)
         //        {
         //            TempData["message"] = "Password changed successfully";
